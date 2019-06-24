972a13838914697028f050712dce5e788b00dc29
eemshi <oreneemshi@gmail.com>
2019-06-23 23:49:55 -0600

    add /search endpoint & server live reload

--- ssr-server.js
+++ ssr-server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const next = require('next')
+const axios = require('axios')
     
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
@@ -8,7 +9,22 @@ const handle = app.getRequestHandler()
 app.prepare()
 .then(() => {
   const server = express()
-    
+
+  server.get('/search', (req, res) => {
+    axios.get('https://api.github.com/search/repositories', {
+        params: {
+            q: req.query.q,
+            sort: req.query.sort,
+            per_page: req.query.per_page
+        }
+    }).then((body) => {
+        // TODO: only return necessary fields
+        return res.json(body.data)
+    }).catch((err) => {
+        return res.status(err.response.status).end(err.response.statusText)
+    })
+  })
+
   server.get('*', (req, res) => {
     return handle(req, res)
   })
