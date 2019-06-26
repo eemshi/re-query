52db53da68fd7794af586a445b061f09c0a7b330
eemshi <oreneemshi@gmail.com>
2019-06-25 23:17:09 -0600

    implement response caching

--- ssr-server.js
+++ ssr-server.js
@@ -8,30 +8,51 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 const REDIS_PORT = 6379
+const client = redis.createClient(REDIS_PORT)
+
+const cacheMiddleware = (req, res, next) => {
+  const query = req.query.q
+  const sort = req.query.sort
+  client.get(query, (err, repositories) => {
+    if (err) res.status(500).end(err.message)
+    if (repositories) {
+      try {
+        const sorted = _.orderBy(JSON.parse(repositories), [sort], ['desc'])
+        res.json(sorted)
+      } catch (e) {
+        res.status(500).end(e.message)
+      }
+    } else {
+      next()
+    }
+  })
+}
+
+const saveCache = (key, ttl=60, value) => {
+  client.setex(key, ttl, JSON.stringify(value))
+}
 
 app.prepare()
 .then(() => {
   const server = express()
-  const client = redis.createClient(REDIS_PORT)
 
-  client.on("error", (err) => {
-    console.log("Error " + err)
+  client.on('error', (err) => {
+    console.log('Error ' + err)
   })
-  client.on("connect", () => {
-    console.log("redis connected")
+  client.on('connect', () => {
+    console.log('redis connected')
   })
 
-  server.get('/search', (req, res) => {
+  server.get('/search', cacheMiddleware, (req, res) => {
+    const query = req.query.q
     const sort = req.query.sort
-    axios.get('https://api.github.com/search/repositories', {
-        params: {
-            q: req.query.q
-        }
-    }).then((body) => {
+    axios.get(`https://api.github.com/search/repositories?q=${query}`)
+    .then((body) => {
         const repositories = body.data.items.map((repo) => {
           return _.pick(repo, 
             ['id', 'full_name', 'language', 'description', 'stargazers_count', 'score', 'url']
           )})
+        saveCache(query, 60, repositories)
         const sorted = _.orderBy(repositories, [sort], ['desc'])
         return res.json(sorted)
     }).catch((err) => {
